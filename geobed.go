c6ff2f77719988c2896515d40b99e12b73240c5d
Mathew Harman <mathew.harman@gmail.com>
2024-03-28 10:00:34 -0400

    use XDG_CACHE_HOME for geobed-data

--- geobed.go
+++ geobed.go
@@ -9,6 +9,7 @@ import (
 	"embed"
 	_ "embed"
 	"encoding/gob"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -17,6 +18,8 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+
+	"github.com/adrg/xdg"
 
 	geohash "github.com/TomiHiltunen/geohash-golang"
 )
@@ -48,11 +51,30 @@ import (
 var cacheData embed.FS
 
 // A list of data sources.
-var dataSetFiles = []map[string]string{
-	{"url": "http://download.geonames.org/export/dump/cities1000.zip", "path": "./geobed-data/cities1000.zip", "id": "geonamesCities1000"},
-	{"url": "http://download.geonames.org/export/dump/countryInfo.txt", "path": "./geobed-data/countryInfo.txt", "id": "geonamesCountryInfo"},
-	//{"url": "http://download.maxmind.com/download/worldcities/worldcitiespop.txt.gz", "path": "./geobed-data/worldcitiespop.txt.gz", "id": "maxmindWorldCities"},
-	//{"url": "http://geolite.maxmind.com/download/geoip/database/GeoLiteCity_CSV/GeoLiteCity-latest.zip", "path": "./geobed-data/GeoLiteCity-latest.zip", "id": "maxmindLiteCity"},
+var dataSetFiles []map[string]string
+
+func init() {
+	cities1000, err := dataSetPath("cities1000.zip")
+	if err != nil {
+		log.Fatalf("err building cache path for cities1000: %v", err)
+	}
+
+	countryInfo, err := dataSetPath("countryInfo.zip")
+	if err != nil {
+		log.Fatalf("err building cache path for countryInfo: %v", err)
+	}
+
+	dataSetFiles = []map[string]string{
+		{"url": "http://download.geonames.org/export/dump/cities1000.zip", "path": cities1000, "id": "geonamesCities1000"},
+		{"url": "http://download.geonames.org/export/dump/countryInfo.txt", "path": countryInfo, "id": "geonamesCountryInfo"},
+		//{"url": "http://download.maxmind.com/download/worldcities/worldcitiespop.txt.gz", "path": "./geobed-data/worldcitiespop.txt.gz", "id": "maxmindWorldCities"},
+		//{"url": "http://geolite.maxmind.com/download/geoip/database/GeoLiteCity_CSV/GeoLiteCity-latest.zip", "path": "./geobed-data/GeoLiteCity-latest.zip", "id": "maxmindLiteCity"},
+	}
+}
+
+func dataSetPath(path string) (string, error) {
+	relPath := fmt.Sprintf("geobed-data/%s", path)
+	return xdg.CacheFile(relPath)
 }
 
 // A handy map of US state codes to full names.
@@ -231,7 +253,6 @@ func NewGeobed() GeoBed {
 
 // Downloads the data sets if needed.
 func (g *GeoBed) downloadDataSets() {
-	os.Mkdir("./geobed-data", 0777)
 	for _, f := range dataSetFiles {
 		_, err := os.Stat(f["path"])
 		if err != nil {
@@ -974,7 +995,7 @@ func (g GeoBed) store() error {
 		return err
 	}
 
-	fh, eopen := os.OpenFile("./geobed-cache/g.c.dmp", os.O_CREATE|os.O_WRONLY, 0666)
+	fh, eopen := os.OpenFile("./geobed-cache/g.c.dmp", os.O_CREATE|os.O_WRONLY, 0o666)
 	defer fh.Close()
 	if eopen != nil {
 		b.Reset()
@@ -996,7 +1017,7 @@ func (g GeoBed) store() error {
 		return err
 	}
 
-	fh, eopen = os.OpenFile("./geobed-cache/g.co.dmp", os.O_CREATE|os.O_WRONLY, 0666)
+	fh, eopen = os.OpenFile("./geobed-cache/g.co.dmp", os.O_CREATE|os.O_WRONLY, 0o666)
 	defer fh.Close()
 	if eopen != nil {
 		b.Reset()
@@ -1018,7 +1039,7 @@ func (g GeoBed) store() error {
 		return err
 	}
 
-	fh, eopen = os.OpenFile("./geobed-cache/cityNameIdx.dmp", os.O_CREATE|os.O_WRONLY, 0666)
+	fh, eopen = os.OpenFile("./geobed-cache/cityNameIdx.dmp", os.O_CREATE|os.O_WRONLY, 0o666)
 	defer fh.Close()
 	if eopen != nil {
 		b.Reset()
