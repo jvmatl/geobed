f69e2c11013efd548002e50c0005c0951dbec82e
John V. Martinez <jvmatl@gmail.com>
2024-05-03 12:29:13 -0400

    Revert "Don't log.Fatal when cache dirs can't be set up"
    This reverts commit c7b4fca8e37e8406898af75ed5c48ca382920057.

--- geobed.go
+++ geobed.go
@@ -56,14 +56,12 @@ var dataSetFiles []map[string]string
 func init() {
 	cities1000, err := dataSetPath("cities1000.zip")
 	if err != nil {
-		log.Printf("err building cache path for cities1000: %v", err)
-		return
+		log.Fatalf("err building cache path for cities1000: %v", err)
 	}
 
 	countryInfo, err := dataSetPath("countryInfo.zip")
 	if err != nil {
-		log.Printf("err building cache path for countryInfo: %v", err)
-		return
+		log.Fatalf("err building cache path for countryInfo: %v", err)
 	}
 
 	dataSetFiles = []map[string]string{
